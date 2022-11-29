48f182bb6c86da0ec301e82d7c1f0e9c31bb78f4
prajjushetty <prajna.shetty@juegostudioz.com>
2022-11-29 11:12:20 +0530

    new response messages are added to response.js

--- src/global/i18n/response.js
+++ src/global/i18n/response.js
@@ -28,6 +28,36 @@ const RESPONSE = {
     responseCode: 1006, responseMessage: {
       "en": "the user is blocked"
     }
+  },
+  ALREADY_REQUESTED: {
+    responseCode: 1007, responseMessage: {
+      "en": "already requested"
+    }
+  },
+  ALREADY_FRIEND: {
+    responseCode: 1008, responseMessage: {
+      "en": "already friend"
+    }
+  },
+  NOT_FRIEND: {
+    responseCode: 1009, responseMessage: {
+      "en": "not your friend"
+    }
+  },
+  ALREADY_REJECTED: {
+    responseCode: 1010, responseMessage: {
+      "en": "already rejected"
+    }
+  },
+  NO_FRIEND_REQUESTS: {
+    responseCode: 1011, responseMessage: {
+      "en": "no friend request"
+    }
+  },
+  NO_FRIENDS: {
+    responseCode: 1012, responseMessage: {
+      "en": "no friends"
+    }
   }
 };
 
