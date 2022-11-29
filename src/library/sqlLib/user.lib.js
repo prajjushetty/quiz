c0bad3c0ed99700c95c73f51b032b6bf00ccd4cc
prajjushetty <prajna.shetty@juegostudioz.com>
2022-11-29 14:07:03 +0530

    updated user.lib library file

--- src/library/sqlLib/user.lib.js
+++ src/library/sqlLib/user.lib.js
@@ -2,41 +2,46 @@ const SQLManager = require('@njs2/sql');
 const { USER_TYPE, STATUS, LEADERBOARD_LIMIT } = require('../../global/constants');
 
 class user {
-    async getUserDetail(whereClause) {
-        return await SQLManager.findOne('user', whereClause);
-    }
+  async getUserDetail(whereClause) {
+    return await SQLManager.findOne('user', whereClause);
+  }
 
-    async getUserList(whereClause) {
-        return await SQLManager.find('user', whereClause);
-    }
+  async getUserList(whereClause) {
+    return await SQLManager.find('user', whereClause);
+  }
 
-    async updateUser(whereClause, updateData) {
-        return await SQLManager.update('user', whereClause, updateData);
-    }
+  async updateUser(whereClause, updateData) {
+    return await SQLManager.update('user', whereClause, updateData);
+  }
 
-    async createUser(userObj) {
-        return await SQLManager.insert('user', userObj);
-    }
+  async createUser(userObj) {
+    return await SQLManager.insert('user', userObj);
+  }
 
-    async getLeaderboard() {
-        const result = await SQLManager.doExecuteRawQuery(`
+  async getLeaderboard() {
+    const result = await SQLManager.doExecuteRawQuery(`
         SELECT * 
         FROM user 
         WHERE user_type=${USER_TYPE.USER} 
         and status=${STATUS.ACTIVE} 
         ORDER BY xp DESC limit ${LEADERBOARD_LIMIT}
          `);
-        return result[0];
-    }
+    return result[0];
+  }
 
-    async getUsers(userIds) {
-        const result = await SQLManager.doExecuteRawQuery(`
+  async getUsers(userIds = []) {
+    if (userIds.length) {
+      const result = await SQLManager.doExecuteRawQuery(`
         SELECT * 
         FROM user
         WHERE user_id IN (${userIds.toString()})
         `);
-        return result[0];
+      return result[0];
+    }else{
+      return [];
     }
+
+  }
 }
 
 module.exports = new user;
